1c9ee8f43d35c5195b61bfd7aa765a6812b77829
jo-bata <jobata@skuniv.ac.kr>
2018-08-09 13:11:17 +0900

    feat: Add login API

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,34 @@ const router = require('express').Router();
 const conn = require('../config/mysql');
 const config = require('../config/config');
 
-router.post('/', function(req, res) {
-
+router.post('/login', function(req, res) {
+  const user = {
+    "id": req.body.id,
+    "nickname": req.body.nickname,
+    "email": req.body.email
+  };
+  const sql = 'SELECT * FROM users WHERE id=?';
+  conn.query(sql, [user.id], function(err, results) {
+    if(err) {
+      console.log(err);
+      res.status(500).json(config.status.sc500);
+    } else if(results.length === 0) {
+      console.log('New user access !');
+      const sql = 'INSERT INTO users SET ?';
+      conn.query(sql, user, function(err, results) {
+        if(err) {
+          console.log(err);
+          res.status(500).json(config.status.sc500);
+        } else {
+          console.log('Add new user !');
+          res.status(200).json(config.status.sc200);
+        }
+      });
+    } else {
+      console.log('Existing user access !');
+      res.status(200).json(config.status.sc200);
+    }
+  });
 });
 
 module.exports = router;
