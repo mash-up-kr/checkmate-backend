2cf994203ed3f102834ee2f73cfd3c7bff0d23fa
jo-bata <jobata@skuniv.ac.kr>
2018-08-09 13:29:45 +0900

    feat: Add add a single workplace API

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,32 @@ router.get('/:userId/work', function(req, res) {
 });
 
 router.post('/:userId/work', function(req, res) {
-
+  func.workIdGenerator(req, req.params.userId, function(workId) {
+    const work = {
+      "id": workId,
+      "user_id": req.params.userId,
+      "name": req.body.name,
+      "address": req.body.address,
+      "latitude": req.body.latitude,
+      "longitude": req.body.longitude,
+      "hourly_wage": req.body.hourly_wage,
+      "probation": req.body.probation,
+      "recess": req.body.recess,
+      "recess_state": req.body.recess_state,
+      "pay_day": req.body.pay_day,
+      "tax": req.body.tax
+    };
+    const sql = 'INSERT INTO works SET ?';
+    conn.query(sql, work, function(err, results) {
+      if(err) {
+        console.log(err);
+        res.status(500).json(config.status.sc500);
+      } else {
+        console.log('Add new work !');
+        res.status(200).json(config.status.sc200);
+      }
+    });
+  });
 });
 
 router.get('/:userId/work/:workId', function(req, res) {
