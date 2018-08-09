b80118c5dc2804f646d429ba9c3e5c83293011f7
jo-bata <jobata@skuniv.ac.kr>
2018-08-10 06:47:33 +0900

    feat: Add total day calculator function

--- config/function.js
+++ config/function.js
@@ -20,5 +20,24 @@ module.exports = {
     const afterDate = (beforeDate.getMonth() > 0) ? new Date(beforeDate.getFullYear(), beforeDate.getMonth(), 0) : new Date(beforeDate.getFullYear() - 1, 12, 0);
     const baseDay = afterDate.getDate();
     callback(baseDay);
+  },
+  totalDayCalculator(res, workId, callback) {
+    const sql = 'SELECT pay_day FROM works WHERE id=?'
+    conn.query(sql, [workId], function(err, results) {
+      if(err | results.length === 0) {
+        console.log(err);
+        res.status(500).json(config.status.sc500);
+      } else {
+        const today = new Date();
+        const beforePayDay = (today.getMonth() > 0) ? new Date(today.getFullYear(), today.getMonth(), results[0].pay_day + 1) : new Date(today.getFullYear() - 1, 12, results[0].pay_day + 1);
+        let diffDate_1 = beforePayDay instanceof Date ? beforePayDay : new Date(beforePayDay);
+        let diffDate_2 = today instanceof Date ? today : new Date(today);
+        diffDate_1 = new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
+        diffDate_2 = new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+        let totalDay = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+        totalDay = Math.ceil(totalDay / (1000 * 3600 * 24));
+        callback(totalDay);
+      }
+    });
   }
 };
