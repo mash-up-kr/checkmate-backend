d7703edae42417073b3c12eca615c070bd88f4ec
jo-bata <jobata@skuniv.ac.kr>
2018-08-13 21:13:08 +0900

    refactor: Modify to match eslintrc

--- config/function.js
+++ config/function.js
@@ -2,18 +2,18 @@ const conn = require('./mysql');
 const config = require('./config');
 
 module.exports = {
-  workIdGenerator (res, userId, callback) {
-   let id = `WO${userId}A`;
-   const sql = "SELECT COUNT(*) AS 'count' FROM works WHERE user_id=?";
-   conn.query(sql, [userId], function(err, results) {
-    if(err) {
-      console.log(err);
-      res.status(500).json(config.status.sc500);
-    } else {
-      id += `${results[0].count}`;
-      callback(id);
-    }
-   });
+  workIdGenerator(res, userId, callback) {
+    let id = `WO${userId}A`;
+    const sql = "SELECT COUNT(*) AS 'count' FROM works WHERE user_id=?";
+    conn.query(sql, [userId], (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json(config.status.sc500);
+      } else {
+        id += `${results[0].count}`;
+        callback(id);
+      }
+    });
   },
   baseDayCalculator(callback) {
     const beforeDate = new Date();
@@ -23,21 +23,21 @@ module.exports = {
   },
   totalDayCalculator(res, workId, callback) {
     const sql = 'SELECT pay_day FROM works WHERE id=?';
-    conn.query(sql, [workId], function(err, results) {
-      if(err | results.length === 0) {
+    conn.query(sql, [workId], (err, results) => {
+      if (err || results.length === 0) {
         console.log(err);
         res.status(500).json(config.status.sc500);
       } else {
         const today = new Date();
         const beforePayDay = (today.getMonth() > 0) ? new Date(today.getFullYear(), today.getMonth(), results[0].pay_day + 1) : new Date(today.getFullYear() - 1, 12, results[0].pay_day + 1);
-        let diffDate_1 = beforePayDay instanceof Date ? beforePayDay : new Date(beforePayDay);
-        let diffDate_2 = today instanceof Date ? today : new Date(today);
-        diffDate_1 = new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
-        diffDate_2 = new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
-        let totalDay = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+        let diffDate1 = beforePayDay instanceof Date ? beforePayDay : new Date(beforePayDay);
+        let difDate2 = today instanceof Date ? today : new Date(today);
+        diffDate1 = new Date(diffDate1.getFullYear(), diffDate1.getMonth() + 1, diffDate1.getDate());
+        difDate2 = new Date(difDate2.getFullYear(), difDate2.getMonth() + 1, difDate2.getDate());
+        let totalDay = Math.abs(difDate2.getTime() - diffDate1.getTime());
         totalDay = Math.ceil(totalDay / (1000 * 3600 * 24));
         callback(totalDay);
       }
     });
-  }
-}; 
+  },
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,23 +3,23 @@ const router = require('express').Router();
 const conn = require('../config/mysql');
 const config = require('../config/config');
 
-router.post('/login', function(req, res) {
+router.post('/login', (req, res) => {
   const user = {
-    "id": req.body.id,
-    "nickname": req.body.nickname,
-    "email": req.body.email
+    id: req.body.id,
+    nickname: req.body.nickname,
+    email: req.body.email,
   };
   const sql = 'SELECT * FROM users WHERE id=?';
-  conn.query(sql, [user.id], function(err, results) {
-    if(err) {
+  conn.query(sql, [user.id], (err, results) => {
+    if (err) {
       console.log(err);
       res.status(500).json(config.status.sc500);
-    } else if(results.length === 0) {
+    } else if (results.length === 0) {
       console.log('New user access !');
-      const sql = 'INSERT INTO users SET ?';
-      conn.query(sql, user, function(err, results) {
-        if(err) {
-          console.log(err);
+      const sql2 = 'INSERT INTO users SET ?';
+      conn.query(sql2, user, (err2) => {
+        if (err2) {
+          console.log(err2);
           res.status(500).json(config.status.sc500);
         } else {
           console.log('Add new user !');
